0c59ea81bac70c999b6c59af0fd8f8fb8e1eca9b
Kalle Kankare <kalle.kankare@iki.fi>
2019-10-01 00:22:21 +0300

    gorels: Progress

--- gorels.go
+++ gorels.go
@@ -24,8 +24,41 @@ func fault(err error, message string, arg ...string) {
 	}
 }
 
+type cmdDesc struct {
+	help string
+	cmd  func(string)
+}
+
+type cmdMap map[string]cmdDesc
+
+func (c *cmdMap) add(name, help string, cmd func(string)) {
+	(*c)[name] = cmdDesc{
+		help: help,
+		cmd:  cmd,
+	}
+}
+
+func newCmdMap() *cmdMap {
+	var ret cmdMap = make(cmdMap)
+
+	ret.add("bump-major", "Bump the major version number", func(s string) {
+	})
+	ret.add("bump-minor", "Bump the minor version number", func(s string) {
+	})
+	ret.add("bump-patch", "Bump the patch level version number", func(s string) {
+	})
+	ret.add("set-version=", "Bump the patch level version number", func(s string) {
+	})
+
+	return &ret
+}
+
+type versionData struct {
+	
+}
+
+
 func main() {
-	optVersion := flag.Bool("v", false, "Display version")
 
 	opts := options.New()
 
@@ -35,10 +68,38 @@ func main() {
 	opts.Set("program-buildgoos", buildGOOS)
 	opts.Set("program-buildgoarch", buildGOARCH)
 
-	flag.Parse()
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	optVersion := fs.Bool("version", false, "Display version")
+	optDebug := fs.Bool("debug", false, "Enable debug output")
+	optDryRun := fs.Bool("dryrun", false, "Don't actually run any commands")
+	optMessage := fs.String("message", "", "String to add to the tag object")
+
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "%s: Tag commits with semantic versions\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Command line options:")
+		fs.PrintDefaults()
+	}
+
+	fs.Parse(os.Args[1:])
 
 	if *optVersion {
 		fmt.Println(options.VersionString(opts))
 		os.Exit(0)
+	}
+
+	if *optDebug {
+		opts.Set("debug", "t")
+	}
+
+	if *optDryRun {
+		opts.Set("dryrun", "t")
+	}
+
+	opts.Set("message", *optMessage)
+
+	args := fs.Args()
+	if len(args) == 0 {
+		fs.Usage()
+		os.Exit(1)
 	}
 }
